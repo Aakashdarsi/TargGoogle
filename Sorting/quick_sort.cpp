3a11721b4b9d99810c967617fabeee6aff2d9dc7
Aakash Darsi <darsisriguruaakash@gmail.com>
2022-05-12 08:39:30 +0530

    :)

---
+++ Sorting/quick_sort.cpp
@@ -0,0 +1,79 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int partition(int arr[],int low,int high)
+{
+    int pivot = arr[0];
+    int smallCount = 0;
+    for(int i = low+1 ; i<=high ;i++)
+    {
+        if(arr[i]<pivot)
+        {
+            smallCount++;
+        }
+    }
+    int pivotIndex = smallCount + low;
+    arr[low] = arr[pivotIndex];
+    arr[pivotIndex] = pivot;
+    int i = low;
+    int j = high;
+    while(i<pivotIndex && j>pivotIndex)
+    {
+        if(arr[i]<=pivot)
+        {
+            i++;
+        }
+        if(arr[j]>pivot)
+        {
+            j--;
+        }
+        else{
+            swap(arr[i++],arr[j--]);
+        }
+    }
+    return pivotIndex;
+    
+    
+}
+
+void quick_sort(int arr[],int low,int high)
+{
+    if(low>=high)
+    {
+        return ;
+    }
+    int p = partition(arr,low,high);
+    quick_sort(arr,low,p-1);
+    quick_sort(arr,p+1,high);
+}
+
+
+int main()
+{
+   int arr[1000];
+    int n;
+    cin>>n;
+    for(int i =0 ;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int low = 0;
+    int high = n-1;
+    quick_sort(arr,low,high);
+    for(int i =0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+
+    return 0;
+}
