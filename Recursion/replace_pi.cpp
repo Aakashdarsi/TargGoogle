369237d91a854554cfb81735315df63da3245a23
Aakash Darsi <darsisriguruaakash@gmail.com>
2022-06-03 23:12:07 +0530

    :)

---
+++ Recursion/replace_pi.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+string replace_pi(string s){
+    if(s.length() == 0 or s.length() == 1 ){
+        return s;
+
+    }
+    if(s[0] == 'p' and s[1] == 'i'){
+        return "3.14"+replace_pi(s.substr(2));
+    }
+    else{
+        string smalPrblm = replace_pi(s.substr(1));
+        return s[0]+smalPrblm;
+    }
+}
+
+
+
+int main(){
+    string s;
+    cin>>s;
+    cout<<replace_pi(s);
+    return 0;
+}
